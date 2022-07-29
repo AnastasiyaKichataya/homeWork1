1288535876067ed2ced7276f5b4138d0e471bf85
KichatayaAB <a.kichataya@w1.money>
2022-07-29 19:20:21 +0300

    first commit

---
+++ src/main/java/com/test/Deposit.java
@@ -0,0 +1,24 @@
+package com.test;
+
+public class Deposit {
+    String currency = "RUB";
+
+    double percent;
+    int period;
+
+
+    public Deposit( int period, double percent) {
+        this.period = period;
+        this.percent = percent;
+
+    }
+
+    public double calculateProfit(int amount){
+
+        double income;
+        income = (amount*percent*period/365)/100;
+        return  income;
+
+    }
+
+}
---
+++ src/main/java/com/test/Money.java
@@ -0,0 +1,10 @@
+package com.test;
+
+public class Money {
+    String currency = "RUB";
+    int amount;
+
+    public Money(int amount) {
+        this.amount = amount;
+    }
+}

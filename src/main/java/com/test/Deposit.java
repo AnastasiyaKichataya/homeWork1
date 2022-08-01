75a24294c0d2bf7539afed6d0739f464608c2fe3
KichatayaAB <a.kichataya@w1.money>
2022-08-01 22:30:45 +0300

    formatting

--- src/main/java/com/test/Deposit.java
+++ src/main/java/com/test/Deposit.java
@@ -2,23 +2,16 @@ package com.test;
 
 public class Deposit {
     String currency = "RUB";
-
     double percent;
     int period;
-
-
-    public Deposit( int period, double percent) {
+    public Deposit(int period, double percent) {
         this.period = period;
         this.percent = percent;
-
     }
 
-    public double calculateProfit(int amount){
-
+    public double calculateProfit(int amount) {
         double income;
-        income = (amount*percent*period/365)/100;
-        return  income;
-
+        income = (amount * percent * period / 365) / 100;
+        return income;
     }
-
 }
--- src/main/java/com/test/MoneyStrategy.java
+++ src/main/java/com/test/MoneyStrategy.java
@@ -2,28 +2,16 @@ package com.test;
 
 public class MoneyStrategy {
     public static void main(String[] args) {
-
-
         Money myMoney = new Money(5000000);
-
         Deposit lucky = new Deposit(365, 75.5);
-
         double income = lucky.calculateProfit(myMoney.amount);
-
         increaseMoney(myMoney.amount, income);
-
     }
-
-    public static void increaseMoney(int amount, double income){
-        if ( amount < 1000000) {
-
+    public static void increaseMoney(int amount, double income) {
+        if (amount < 1000000) {
             System.out.println("Дохода нет");
-        }
-        else {
+        } else {
             System.out.println("Ваш доход составляет:" + income);
         }
-
     }
-
-
 }
